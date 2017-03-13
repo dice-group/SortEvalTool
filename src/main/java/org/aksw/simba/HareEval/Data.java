94b770ac2c92a01bb2ab64fa43b4b658ca3c10fa
Kunal-Jha <kunal94jha@gmail.com>
2017-03-13 16:36:11 +0100

    first commit

---
+++ src/main/java/org/aksw/simba/HareEval/Data.java
@@ -0,0 +1,19 @@
+package org.aksw.simba.HareEval;
+
+public class Data {
+
+	String uri;
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public Data() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
