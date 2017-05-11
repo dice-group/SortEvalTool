8af0bdb0d5c1bc5a5bf4ee80991eab2b67253677
Kunal-Jha <kunal94jha@gmail.com>
2017-05-11 23:51:56 +0200

    Fixed the problem for equals

--- src/main/java/org/aksw/simba/hare/input/StatGeneration.java
+++ src/main/java/org/aksw/simba/hare/input/StatGeneration.java
@@ -1,28 +1,14 @@
 package org.aksw.simba.hare.input;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
-
 import org.aksw.simba.hare.model.Data;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.h2.command.dml.Set;
-
-import com.hp.hpl.jena.query.ParameterizedSparqlString;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class StatGeneration {
 
@@ -35,56 +21,47 @@ public class StatGeneration {
 		ArrayList<Data> prResult = new ArrayList<Data>();
 		prResult = hr.getPRResults();
 
-		BufferedWriter out = new BufferedWriter(new FileWriter("output.txt"));
+		sg.writeToFile(sg.calcuatePRSetDifference(sg.generatePair(hareResult), sg.generatePair(prResult)), "PAGERANK");
+		sg.writeToFile(sg.calcuateHareSetDifference(sg.generatePair(hareResult), sg.generatePair(prResult)), "HARE");
 
-		HashMap<String, ArrayList<Pairs>> setDiff = sg.calcuateSetDifference(sg.generatePairs(hareResult, "HARE"),
-				sg.generatePairs(prResult, "PAGERANK"));
+	}
 
-		for (String key : setDiff.keySet()) {
+	public void writeToFile(HashMap<String, ArrayList<Pair<String, String>>> setDiffPR, String result)
+			throws IOException {
+		BufferedWriter out = new BufferedWriter(new FileWriter("output_"+result+".txt"));
+
+		for (String key : setDiffPR.keySet()) {
+			out.write(result + "\n");
+			out.write("=============" + "\n");
 			out.write(key + "\n");
 			out.write("======================================" + "\n");
-			ArrayList<Pairs> res = setDiff.get(key);
-			for (Pairs ele : res) {
-				out.write(ele.resultFrom + "\n");
+			ArrayList<Pair<String, String>> res = setDiffPR.get(key);
+			for (Pair<String, String> ele : res) {
 				out.write("=============" + "\n");
-				out.write(ele.left.toString() + "\n");
-				out.write(ele.right.toString() + "\n");
+				out.write(ele.getLeft().toString() + "\n");
+				out.write(ele.getRight().toString() + "\n");
 				out.write("============END OF PAIR=====================" + "\n");
 			}
 			out.write("============END OF CATEGORY=====================" + "\n\n");
 		}
-
 	}
 
-	public HashMap<String, ArrayList<Pairs>> calcuateSetDifference(HashMap<String, ArrayList<Pairs>> hare,
-			HashMap<String, ArrayList<Pairs>> pr) {
-
-		HashMap<String, ArrayList<Pairs>> setDifference = new HashMap<String, ArrayList<Pairs>>();
-
+	public HashMap<String, ArrayList<Pair<String, String>>> calcuatePRSetDifference(
+			HashMap<String, ArrayList<Pair<String, String>>> hare,
+			HashMap<String, ArrayList<Pair<String, String>>> pr) {
+		HashMap<String, ArrayList<Pair<String, String>>> setDifference = new HashMap<String, ArrayList<Pair<String, String>>>();
 		for (String key : pr.keySet()) {
 			if (hare.containsKey(key)) {
-				ArrayList<Pairs> prUriList = pr.get(key);
-				ArrayList<Pairs> hareUriList = hare.get(key);
+				ArrayList<Pair<String, String>> prUriList = pr.get(key);
+				ArrayList<Pair<String, String>> hareUriList = hare.get(key);
 				System.out.println(key);
 				System.out.println("pR LIST " + prUriList.size());
-				System.out.println("hare LIST " + hareUriList.size());
-				ArrayList<Pairs> res = new ArrayList<Pairs>(prUriList) ;
-				ArrayList<Pairs> res1 = new ArrayList<Pairs>(hareUriList);
-				
-				res.removeAll(hareUriList);
-				res1.removeAll(prUriList);
-				/*
-				for (Pairs e : prUriList) {
-					if (existinList(hareUriList, e)) {
-						res.remove(e);
+				ArrayList<Pair<String, String>> res = new ArrayList<Pair<String, String>>(prUriList);
+				for (Pair<String, String> ele : prUriList) {
+					if (existinList(hareUriList, ele)) {
+						res.remove(ele);
 					}
 				}
-				for (Pairs e : hareUriList) {
-					if (existinList(prUriList, e)) {
-						res1.remove(e);
-					}
-				}*/
-				res.addAll(res1);
 				System.out.println("Final");
 				System.out.println(res.size());
 				setDifference.put(key, res);
@@ -93,21 +70,44 @@ public class StatGeneration {
 		return setDifference;
 	}
 
-	public HashMap<String, ArrayList<Pairs>> generatePairs(ArrayList<Data> list, String resultFrom) {
-		HashMap<String, ArrayList<Pairs>> hmap = new HashMap<String, ArrayList<Pairs>>();
+	public HashMap<String, ArrayList<Pair<String, String>>> calcuateHareSetDifference(
+			HashMap<String, ArrayList<Pair<String, String>>> hare,
+			HashMap<String, ArrayList<Pair<String, String>>> pr) {
+		HashMap<String, ArrayList<Pair<String, String>>> setDifference = new HashMap<String, ArrayList<Pair<String, String>>>();
+		for (String key : pr.keySet()) {
+			if (hare.containsKey(key)) {
+				ArrayList<Pair<String, String>> prUriList = pr.get(key);
+				ArrayList<Pair<String, String>> hareUriList = hare.get(key);
+				System.out.println(key);
+				System.out.println("hare LIST " + hareUriList.size());
+				ArrayList<Pair<String, String>> res1 = new ArrayList<Pair<String, String>>(hareUriList);
+				for (Pair<String, String> ele : hareUriList) {
+					if (existinList(prUriList, ele)) {
+						res1.remove(ele);
+					}
+				}
+				System.out.println("Final");
+				System.out.println(res1.size());
+				setDifference.put(key, res1);
+			}
+		}
+		return setDifference;
+	}
+
+	public HashMap<String, ArrayList<Pair<String, String>>> generatePair(ArrayList<Data> list) {
+		HashMap<String, ArrayList<Pair<String, String>>> hmap = new HashMap<String, ArrayList<Pair<String, String>>>();
 
 		for (Data cat : list) {
-			ArrayList<Pairs> uriList = new ArrayList<Pairs>();
+			ArrayList<Pair<String, String>> uriList = new ArrayList<Pair<String, String>>();
 			for (String ele : cat.getUri()) {
 				for (String ele1 : cat.getUri()) {
 					if (!ele.equals(ele1)) {
-						Pairs newPair = new Pairs(ele1, ele, resultFrom);
+						Pair<String, String> newPair = new ImmutablePair<String, String>(ele, ele1);
 						if (uriList.size() == 0) {
 							uriList.add(newPair);
 						} else {
 							if (!existinList(uriList, newPair))
-								System.out.println("Pair   size " + newPair.toString());
-							uriList.add(newPair);
+								uriList.add(newPair);
 						}
 					}
 				}
@@ -120,10 +120,12 @@ public class StatGeneration {
 
 	}
 
-	public boolean existinList(ArrayList<Pairs> list, Pairs newPair) {
-		for (Pairs ele : list) {
-			if (ele.equals(newPair))
+	public boolean existinList(ArrayList<Pair<String, String>> list, Pair<String, String> newPair) {
+		for (Pair<String, String> ele : list) {
+			if ((ele.getLeft().equals(newPair.getLeft()) && (ele.getRight().equals(newPair.getRight())))
+					|| ((ele.getRight().equals(newPair.getLeft())) && (ele.getLeft().equals(newPair.getRight())))) {
 				return true;
+			}
 		}
 
 		return false;
